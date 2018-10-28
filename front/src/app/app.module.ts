1b5f2307e84530ac1d6f7b5f892dfc79a85d49bd
abrar zahin <abrarzahin@yahoo.com>
2018-10-28 03:07:18 -0400

    1st

---
+++ front/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import{HttpClientModule} from '@angular/common/http'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatButtonModule, MatInputModule, MatCardModule } from '@angular/material'
+import{FormsModule} from '@angular/forms'
+import { AppComponent } from './app.component'
+import { QuestionComponent } from './question.component'
+import { ApiService } from './api.service'
+@NgModule({
+  declarations: [
+    AppComponent, QuestionComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    HttpClientModule
+  ],
+  providers: [ApiService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
